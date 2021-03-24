f7fa0ac0d3a405b6b731c6628e8d244727c13cc3
Nicosqui <nico50829@gmail.com>
2021-03-23 20:49:30 -0500

    first commit

---
+++ Sum_of_digits/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Sum_of_digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un número:");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine("La suma de los digitos de " + number + " es: " + SumDigits(number));
+        }
+
+        static int SumDigits(int n)
+        {
+            //base case:
+            if (n < 10)
+            {
+                return n;
+            }
+            else
+            {
+                return (n % 10) + SumDigits(n / 10);
+            }
+        }
+    }
+}
